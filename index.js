083fac9f7879d4f86bf29fdaeb79b40fe09140f0
Daniel Santana <github@danielsantana.net>
2019-05-28 14:25:25 -0400

    standardize linting with standard.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-const makeSureThat = require('assert');
+const makeSureThat = require('assert')
 
 // this function allows main modules to be tested
 const run = (filename, init, ...args) => {
   makeSureThat(init instanceof Function,
-    `Expecting second argument as a Function but received ${typeof init}`);
+    `Expecting second argument as a Function but received ${typeof init}`)
 
   if (process.argv[1] === filename) {
-    return init(...args);
+    return init(...args)
   }
-};
+}
 
-module.exports = { run };
+module.exports = { run }
--- test/index.test.js
+++ test/index.test.js
@@ -1,71 +1,72 @@
-'require strict';
+'require strict'
 
 /* eslint-env mocha */
 /* eslint-disable no-unused-expressions */
 
 const
-  sinon = require('sinon'),
-  { expect } = require('chai');
+  sinon = require('sinon')
+
+const { expect } = require('chai')
 
 describe('runner.js', () => {
-  const noop = () => { };
+  const noop = () => { }
 
-  let sandbox;
+  let sandbox
   beforeEach(() => {
     // creates sinon sandbox
-    sandbox = sinon.sandbox.create();
-  });
+    sandbox = sinon.sandbox.create()
+  })
 
   // retores the sandbox
-  afterEach(() => { sandbox.restore(); });
+  afterEach(() => { sandbox.restore() })
 
   context('#run()', () => {
     context('when filename is NOT the second arg', () => {
       beforeEach(() => {
-        sandbox.stub(process, 'argv').value([null, 'this is not a file name']);
-      });
+        sandbox.stub(process, 'argv').value([null, 'this is not a file name'])
+      })
 
       it('should NOT call the function', () => {
-        const { run } = require('../');
-        const callback = sinon.spy(noop);
-        run(__filename, callback);
+        const { run } = require('../')
+        const callback = sinon.spy(noop)
+        run(__filename, callback)
 
-        expect(callback.called).to.be.false;
-      });
-    });
+        expect(callback.called).to.be.false
+      })
+    })
 
     context('when filename is the second arg', () => {
       beforeEach(() => {
-        sandbox.stub(process, 'argv').value([null, __filename]);
-      });
+        sandbox.stub(process, 'argv').value([null, __filename])
+      })
 
       it('should call the function', () => {
-        const { run } = require('../');
-        const callback = sinon.spy(noop);
-        run(__filename, callback);
+        const { run } = require('../')
+        const callback = sinon.spy(noop)
+        run(__filename, callback)
 
-        expect(callback.calledOnce).to.be.true;
-      });
+        expect(callback.calledOnce).to.be.true
+      })
 
       it('should call the function with the expected 3 arguments', () => {
-        const { run } = require('../');
-        const callback = sinon.spy(noop);
-        run(__filename, callback, 2, 4, 6);
+        const { run } = require('../')
+        const callback = sinon.spy(noop)
+        run(__filename, callback, 2, 4, 6)
 
-        expect(callback.calledOnce).to.be.true;
-        expect(callback.calledWithExactly(2, 4, 6)).to.be.true;
-      });
+        expect(callback.calledOnce).to.be.true
+        expect(callback.calledWithExactly(2, 4, 6)).to.be.true
+      })
 
       it('should call the function with the expected N arguments', () => {
-        const { run } = require('../');
-        const callback = sinon.spy(noop);
-        const length = ~~(Math.random() * 10);
-        const nArgs = Array.from({ length }, (v, i) => i);
-        run(__filename, callback, ...nArgs);
+        const { run } = require('../')
+        const callback = sinon.spy(noop)
+        const length = ~~(Math.random() * 10)
+        const nArgs = Array.from({ length }, (v, i) => i)
+        run(__filename, callback, ...nArgs)
 
-        expect(callback.calledOnce).to.be.true;
-        expect(callback.calledWithExactly(...nArgs)).to.be.true;
-      });
-    });
-  });
-});
+        expect(callback.calledOnce).to.be.true
+        expect(callback.calledWithExactly(...nArgs)).to.be.true
+      })
+    })
+  })
+})
